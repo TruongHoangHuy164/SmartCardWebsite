b835abc3db0dbe892e885271b21513d2b82eeaa0
hau <nguyenviethau.it.2004@gmail.com>
2025-07-18 16:50:27 +0700

    Update code:thêm chấm điểm và xử lý ảnh

--- flashcard/flashcard/src/main/java/com/quizletclone/flashcard/service/QuizService.java
+++ flashcard/flashcard/src/main/java/com/quizletclone/flashcard/service/QuizService.java
@@ -1,12 +1,13 @@
 package com.quizletclone.flashcard.service;
 
-import com.quizletclone.flashcard.model.Quiz;
-import com.quizletclone.flashcard.repository.QuizRepository;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import com.quizletclone.flashcard.model.Quiz;
+import com.quizletclone.flashcard.repository.QuizRepository;
 
 @Service
 public class QuizService {
@@ -28,4 +29,14 @@ public class QuizService {
     public void deleteQuiz(Integer id) {
         quizRepository.deleteById(id);
     }
-} 
+
+    public int getCorrectAnswersFromLatestQuiz(int userId, int deckId) {
+        Optional<Quiz> quizOpt = quizRepository.findTopByUserIdAndDeckIdOrderByCreatedAtDesc(userId, deckId);
+        if (quizOpt.isPresent()) {
+            Quiz quiz = quizOpt.get();
+            Integer correctAnswers = quiz.getCorrectAnswers();
+            return (correctAnswers != null) ? correctAnswers : 0;
+        }
+        return 0;
+    }
+}
