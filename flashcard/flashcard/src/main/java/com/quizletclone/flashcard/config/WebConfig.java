9e4330be9d84ea8eab24e51051d0e8a8f9820d8f
hau <nguyenviethau.it.2004@gmail.com>
2025-07-21 20:39:26 +0700

    Up code lên git

--- flashcard/flashcard/src/main/java/com/quizletclone/flashcard/config/WebConfig.java
+++ flashcard/flashcard/src/main/java/com/quizletclone/flashcard/config/WebConfig.java
@@ -2,10 +2,13 @@ package com.quizletclone.flashcard.config;
 
 import java.nio.file.Paths;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.quizletclone.flashcard.interceptor.UserStatusInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
--- flashcard/flashcard/src/main/java/com/quizletclone/flashcard/repository/DeckRepository.java
+++ flashcard/flashcard/src/main/java/com/quizletclone/flashcard/repository/DeckRepository.java
@@ -7,10 +7,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.quizletclone.flashcard.model.Deck;
+import com.quizletclone.flashcard.model.User;
 
 public interface DeckRepository extends JpaRepository<Deck, Integer> {
     List<Deck> findByIsPublicTrue();
+
     Page<Deck> findByTitleContainingIgnoreCase(String keyword, Pageable pageable);
+
     Page<Deck> findBySubjectIgnoreCase(String subject, Pageable pageable);
+
     Page<Deck> findByTitleContainingIgnoreCaseAndSubjectIgnoreCase(String keyword, String subject, Pageable pageable);
+
+    List<Deck> findByUser(User user);
+
+    List<Deck> findByUserNot(User user);
 }
